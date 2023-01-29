4e4cc093a338b496b1058c8b6cc493aab8451970
Sharun <sharunksplus@gmail.com>
2023-01-29 00:04:49 -0500

    solution for day 2

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,14 +1,21 @@
 export abstract class Solution {
     // define an abstract method that solves
     abstract solve(input: string): any
+    get_lines(input: string): Array<string> {
+        return input.split("\r\n")
+    }
+    sum(x: number, y: number): number {
+        return x + y
+    }
 }
 
 import { readFileSync, writeFileSync } from "fs";
 import CalorieCounting from "./day-1-calorie-counting-2";
+import RockPaperScissors from "./day-2-rock-paper-scissors-1";
 
 const input = readFileSync(__dirname + "/io/input.txt", "utf-8")
 
-const solution: Solution = new CalorieCounting()
+const solution: Solution = new RockPaperScissors()
 
 const output = solution.solve(input)
 
