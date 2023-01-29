007937ace63b4d133c46539db4f94fb697dc7254
Sharun <sharunksplus@gmail.com>
2023-01-29 00:32:59 -0500

    solution for day 2, part 2

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -11,7 +11,7 @@ export abstract class Solution {
 
 import { readFileSync, writeFileSync } from "fs";
 import CalorieCounting from "./day-1-calorie-counting-2";
-import RockPaperScissors from "./day-2-rock-paper-scissors-1";
+import RockPaperScissors from "./day-2-rock-paper-scissors-2";
 
 const input = readFileSync(__dirname + "/io/input.txt", "utf-8")
 
