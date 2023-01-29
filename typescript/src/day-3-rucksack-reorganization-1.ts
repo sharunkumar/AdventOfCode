c9611da9be021682ee18361d0babd5c3a7b81110
Sharun <sharunksplus@gmail.com>
2023-01-29 01:58:46 -0500

    solution for day 3

---
+++ typescript/src/day-3-rucksack-reorganization-1.ts
@@ -0,0 +1,36 @@
+import { ascii_to_char, char_to_ascii, pipelog, Solution, sum } from "./utils"
+
+export default class RucksackReorganization extends Solution {
+    constructor() {
+        super()
+    }
+
+    split_rucksack(items: string): string[] {
+        return [
+            items.slice(0, items.length / 2),
+            items.slice(items.length / 2)
+        ]
+    }
+
+    intersect(one: string, two: string): string {
+        return Array.from(one).filter(item => Array.from(two).includes(item))[0]
+    }
+
+    get_priority(char: string): number {
+        let expr = char_to_ascii(char) - char_to_ascii('a') + 1
+
+        if (expr < 1) {
+            expr = char_to_ascii(char) - char_to_ascii('A') + 27
+        }
+
+        return expr
+    }
+
+    solve(input: string): any {
+        return this.get_lines(input)
+            .map(this.split_rucksack)
+            .map(([one, two]) => this.intersect(one, two))
+            .map(this.get_priority)
+            .reduce(sum)
+    }
+}
