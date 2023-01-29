f82486ce727d68363c00cd4c1d58f78239a357ce
Sharun <sharunksplus@gmail.com>
2023-01-29 13:27:14 -0500

    solution for day 4

---
+++ typescript/src/day-4-camp-cleanup-1.ts
@@ -0,0 +1,28 @@
+import { pipelog, Solution, sum } from "./utils";
+
+class Range extends Object {
+    start: number;
+    end: number;
+    constructor(range: string) {
+        super()
+        let splits = range.split("-")
+        this.start = parseInt(splits[0])
+        this.end = parseInt(splits[1])
+    }
+
+    includes(other: Range): boolean {
+        return this.start <= other.start && this.end >= other.end
+    }
+}
+
+export default class CampCleanup extends Solution {
+
+    solve(input: string) {
+        return this.get_lines(input)
+            .map(str => str.split(","))
+            .map(arr => [new Range(arr[0]), new Range(arr[1])])
+            .filter(ranges => ranges[0].includes(ranges[1]) || ranges[1].includes(ranges[0]))
+            .map(pipelog)
+            .length
+    }
+}
