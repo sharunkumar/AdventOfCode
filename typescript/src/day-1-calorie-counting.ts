a2ccdf155be6d9d0f7aa0a52258d2ee8e9122df6
Sharun <sharunksplus@gmail.com>
2023-01-28 22:58:30 -0500

    scaffolding for i/o operations

---
+++ typescript/src/day-1-calorie-counting.ts
@@ -0,0 +1,9 @@
+import { Solution } from ".";
+
+export default class CalorieCounting extends Solution {
+    solve(input: string): string {
+
+
+        return input
+    }
+}
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,2 +1,15 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+export abstract class Solution {
+    // define an abstract method that solves
+    abstract solve(input: string): string
+}
+
+import { readFileSync, writeFileSync } from "fs";
+import CalorieCounting from "./day-1-calorie-counting";
+
+const input = readFileSync(__dirname + "/io/input.txt", "utf-8")
+
+const solution: Solution = new CalorieCounting()
+
+const output = solution.solve(input)
+
+writeFileSync(__dirname + "/io/output.txt", output)
