7789b234f60326c0232419f87c49735069d82f2e
Sharun <sharunksplus@gmail.com>
2023-01-29 16:41:51 -0500

    updated index to setup input files based on class name

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,13 +1,26 @@
-import { readFileSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import path from "path";
 import CalorieCounting from "./solutions/day-1-calorie-counting-2";
 import RockPaperScissors from "./solutions/day-2-rock-paper-scissors-2";
 import RucksackReorganization from "./solutions/day-3-rucksack-reorganization-2";
 import CampCleanup from "./solutions/day-4-camp-cleanup-2";
 import { Solution } from "./utils";
 
-const solution: Solution = new CampCleanup()
+const solution: Solution = new RucksackReorganization()
 
-const input = readFileSync(__dirname + "/io/input.txt", "utf-8")
+const input_file_name = path.join(__dirname, "io", `${solution.constructor.name}.txt`)
+
+if (!existsSync(input_file_name)) {
+    writeFileSync(input_file_name, "", "utf-8")
+    console.debug(`${input_file_name} created.`)
+}
+
+const input = readFileSync(input_file_name, "utf-8")
+
+if (input == "") {
+    console.debug(`add input to ${input_file_name} to continue.`)
+    process.exit(0)
+}
 
 const start = Date.now()
 
