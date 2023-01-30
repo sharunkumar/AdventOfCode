b0d26435464036aff578d7b2d6051fc02b4e917b
Sharun <sharunksplus@gmail.com>
2023-01-29 19:51:36 -0500

    solution for day 6, part 2

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,7 +5,7 @@ import RockPaperScissors from "./solutions/day-2-rock-paper-scissors-2";
 import RucksackReorganization from "./solutions/day-3-rucksack-reorganization-2";
 import CampCleanup from "./solutions/day-4-camp-cleanup-2";
 import SupplyStacks from "./solutions/day-5-supply-stacks-2";
-import TuningTrouble from "./solutions/day-6-tuning-trouble-1";
+import TuningTrouble from "./solutions/day-6-tuning-trouble-2";
 import { Solution } from "./utils";
 
 const solution: Solution = new TuningTrouble()
---
+++ typescript/src/solutions/day-6-tuning-trouble-2.ts
@@ -0,0 +1,20 @@
+import { Solution } from "../utils";
+
+export default class TuningTrouble extends Solution {
+    solve(input: string) {
+
+        let index = -1
+        let size = 14
+
+        for (let i = 0; i <= input.length - size; i++) {
+            let slice = input.substring(i, i + size)
+
+            if (slice.length == new Set(slice).size) {
+                index = i
+                break
+            }
+        }
+
+        return index + size
+    }
+}
