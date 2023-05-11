d022dcff28002301132c3e2c811478535760003e
Sharun <sharunksplus@gmail.com>
2023-05-10 22:31:29 -0400

    refactor

--- typescript/src/solutions/day-10-cathode-ray-tube.ts
+++ typescript/src/solutions/day-10-cathode-ray-tube.ts
@@ -1,29 +1,32 @@
 import { Solution, pipelog } from "../utils";
 
-class I {
-    static cycles = 0
-    static x = 1
-    static signal = 0
+let cycles = 0
+let x = 1
+let signal = 0
+
+function increment_cycle() {
+    cycles++
+    if (is_qualifying_cycle()) {
+        signal += cycles * x
+        console.log(cycles, x)
+    }
+}
+
+function is_qualifying_cycle() {
+    return cycles >= 20 && (cycles - 20) % 40 == 0
+}
+
+class Instruction {
     static noop() {
         console.log("noop")
-        I.increment_cycle()
+        increment_cycle()
     }
 
     static addx(x: number) {
         console.log("addx", x)
-        I.increment_cycle()
-        I.increment_cycle()
-        I.x += x
-    }
-    static increment_cycle() {
-        I.cycles++
-        if (I.is_qualifying_cycle()) {
-            I.signal += I.cycles * I.x
-            console.log(I.cycles, I.x)
-        }
-    }
-    static is_qualifying_cycle() {
-        return I.cycles >= 20 && (I.cycles - 20) % 40 == 0
+        increment_cycle()
+        increment_cycle()
+        x += x
     }
 }
 
@@ -34,12 +37,12 @@ export default class CathodeRayTube extends Solution {
             .map(line => line.split(" "))
             .map(command => {
                 let func = command.shift()
-                return [Reflect.get(I, func + ""), ...command.map(parseInt)]
+                return [Reflect.get(Instruction, func + ""), ...command.map(parseInt)]
             })
             .map(([func, ...args]) => {
                 func(...args)
             })
 
-        return I.signal
+        return signal
     }
 }
