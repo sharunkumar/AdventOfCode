8cef3f26842e343e7cd6f83d6ea9c987fd03cfce
Sharun <sharunksplus@gmail.com>
2023-05-10 20:35:13 -0400

    final solution for part 1

--- typescript/src/solutions/day-9-rope-bridge-1.ts
+++ typescript/src/solutions/day-9-rope-bridge-1.ts
@@ -55,21 +55,29 @@ class GameState {
 
     move_head(move: MoveD) {
         let head = this.knots[0]
-        let tail = this.knots[1]
         for (let i = 0; i < move.count; i++) {
             head.x += move.dx;
             head.y += move.dy;
 
-            if (!tail.is_around(head)) {
-                let tdx = head.x == tail.x ? 0 : (head.x - tail.x) / Math.abs(head.x - tail.x)
-                let tdy = head.y == tail.y ? 0 : (head.y - tail.y) / Math.abs(head.y - tail.y)
+            let ti = 1
+            do {
+                let tail = this.knots[ti]
+                let head = this.knots[ti - 1]
+                if (!tail.is_around(this.knots[ti - 1])) {
+                    let tdx = head.x == tail.x ? 0 : (head.x - tail.x) / Math.abs(head.x - tail.x)
+                    let tdy = head.y == tail.y ? 0 : (head.y - tail.y) / Math.abs(head.y - tail.y)
 
-                tail.x += tdx;
-                tail.y += tdy;
-            }
+                    tail.x += tdx;
+                    tail.y += tdy;
+                }
 
-            if (tail == this.knots[this.knots.length - 1])
-                this.tail_states.add([tail.x, tail.y])
+                if (tail == this.knots[this.knots.length - 1]) {
+                    this.tail_states.add([tail.x, tail.y])
+                    break
+                } else {
+                    ti++
+                }
+            } while (true)
         }
     }
 }
