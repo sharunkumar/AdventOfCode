be5b6ca38b8564312e22c2cc5229dd6fc3bcd412
Sharun <sharunksplus@gmail.com>
2023-05-12 05:48:09 -0400

    added helper functions for position, elevation, etc

--- typescript/src/solutions/day-12-hill-climbing-algorithm-1.ts
+++ typescript/src/solutions/day-12-hill-climbing-algorithm-1.ts
@@ -2,7 +2,7 @@ import { Solution, ascii_lowercase } from "../utils";
 import { PriorityQueue } from 'p-queue-ts';
 
 class Position {
-    constructor(public x: number, public y: number) { }
+    constructor(public x: number, public y: number, public z: number, public steps: number = 0) { }
 }
 
 function get_elevation(char: string) {
@@ -19,11 +19,15 @@ function get_elevation(char: string) {
 
 export default class HillClimbingAlgorithm extends Solution {
     solve(input: string) {
-        let S = new Position(0, 0)
-        let E = new Position(0, 0)
+        let S = new Position(0, 0, 0)
+        let E = new Position(0, 0, 0)
         const matrix = this.get_lines(input)
             // .pipelog()
             .map(l => l.split(""))
+
+        function position(i: number, j: number, steps: number = 0) {
+            return new Position(i, j, get_elevation(matrix[i][j]), steps)
+        }
 
         for (let i = 0; i < matrix.length; i++) {
             const line = matrix[i];
@@ -31,16 +35,23 @@ export default class HillClimbingAlgorithm extends Solution {
                 const char = line[j];
                 // console.log({ char, i, j })
                 if (char == "S") {
-                    S = new Position(i, j)
+                    S = position(i, j)
                 }
                 if (char == "E") {
-                    E = new Position(i, j)
+                    E = position(i, j, 10)
                 }
             }
         }
 
         const elevation = matrix.map(line => line.map(get_elevation))//.pipelog()
         const visited = matrix.map(line => line.map(c => false))//.pipelog()
+
+        const pq = new PriorityQueue((a: Position, b: Position) => a.steps > b.steps)
+
+        pq.push(S)
+        pq.push(E)
+
+        console.log(pq.pop())
 
         return { matrix, S, E }
     }
