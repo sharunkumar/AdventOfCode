4e62b2de230fef06c056cb13e63b18b5cb048ccd
Sharun <sharunksplus@gmail.com>
2023-05-16 00:05:12 -0400

    init part 2

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -13,7 +13,7 @@ import RopeBridge from "./solutions/day-09-rope-bridge-2";
 import CathodeRayTube from "./solutions/day-10-cathode-ray-tube-2";
 import MonkeyInTheMiddle from "./solutions/day-11-monkey-in-the-middle-2";
 import HillClimbingAlgorithm from "./solutions/day-12-hill-climbing-algorithm-2";
-import DistressSignal from "./solutions/day-13-distress-signal-1";
+import DistressSignal from "./solutions/day-13-distress-signal-2";
 
 const solution: Solution = new DistressSignal()
 
