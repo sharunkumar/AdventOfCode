56b18fe951674940f3e3b92b2f70d23feb535840
Sharun <sharunksplus@gmail.com>
2023-05-15 22:00:20 -0400

    updated algo

--- typescript/src/solutions/day-13-distress-signal-1.ts
+++ typescript/src/solutions/day-13-distress-signal-1.ts
@@ -17,36 +17,40 @@ export default class DistressSignal extends Solution {
             if (Number.isFinite(l) && Number.isFinite(r)) {
                 if (l == r) continue
                 else {
-                    if (l < r) return 1
-                    else return 0
+                    if (l < r) return -1
+                    else return 1
                 }
             }
             else if (Array.isArray(l) && Array.isArray(r)) {
                 let result = this.compare_signals(l, r)
-                if (!result) return 0
+                if (result == 0) continue
+                else return result
             }
             else {
                 if (Number.isFinite(l)) {
                     let result = this.compare_signals([l] as Signal, r)
-                    if (!result) return 0
+                    if (result == 0) continue
+                    else return result
                 } else if (Number.isFinite(r)) {
                     let result = this.compare_signals(l, [r] as Signal)
-                    if (!result) return 0
+                    if (result == 0) continue
+                    else return result
                 }
             }
         }
 
         console.log({ i })
-        if (i == rl && ll != rl) { return 0 }
-        return 1
+        if (i == rl && ll != rl) { return 1 }
+        return -1
     }
 
     solve(input: string) {
         let signals = this.get_blocks(input)
             .map(block => this.get_lines(block)
                 .map(line => JSON.parse(line)) as [Signal, Signal])
-            .map((sigs, idx) => this.compare_signals(sigs[0], sigs[1]) == 1 ? idx + 1 : 0)
-            .pipelog()
+            .map((sigs, idx) => this.compare_signals(sigs[0], sigs[1]))
+            .pipelog(true, 1)
+            .map((x, idx) => x == -1 ? idx + 1 : 0)
             .sum()
 
         return { signals }
