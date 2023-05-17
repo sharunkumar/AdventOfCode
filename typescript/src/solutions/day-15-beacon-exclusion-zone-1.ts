9ba6edac713bc9d1162ba0fb8eb47807b51f7c94
Sharun <sharunksplus@gmail.com>
2023-05-17 01:35:08 -0400

    init part 1

---
+++ typescript/src/solutions/day-15-beacon-exclusion-zone-1.ts
@@ -0,0 +1,9 @@
+import { Solution } from "../utils";
+
+export default class BeaconExclusionZone extends Solution {
+    solve(input: string) {
+        this.get_lines(input)
+            .pipelog()
+    }
+
+}
