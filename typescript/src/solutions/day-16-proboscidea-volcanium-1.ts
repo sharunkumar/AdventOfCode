19174d24837ea728be87d977c2393b5288919608
Sharun <sharunksplus@gmail.com>
2023-05-17 19:30:02 -0400

    typings updated

--- typescript/src/solutions/day-16-proboscidea-volcanium-1.ts
+++ typescript/src/solutions/day-16-proboscidea-volcanium-1.ts
@@ -1,17 +1,26 @@
 import { Solution, regexMatch } from "../utils";
 
+type valve = string
+interface distance { [key: valve]: { [key: valve]: number } }
+interface valves { [key: valve]: number }
+interface tunnels { [key: valve]: valve[] }
+
 export default class ProboscideaVolcanium extends Solution {
     solve(input: string) {
-        let valves = new Map<string, number>()
-        let tunnels = new Map<string, string[]>()
+
+        let valves: valves = {}
+        let tunnels: tunnels = {}
+        let dist: distance = {}
+        let nonempty = [] as valve[]
         this.get_lines(input)
             .map(line => regexMatch(line, /[A-Z0-9]+/g))
-            .pipelog()
             .forEach(([_, valve, rate, ...targets]) => {
-                valves.set(valve, Number(rate))
-                tunnels.set(valve, targets)
+                valves[valve] = Number(rate)
+                tunnels[valve] = targets
             })
 
-        console.log({ valves, tunnels })
+        for (let x in valves) {
+            console.log({ x, v: valves[x] })
+        }
     }
 }
