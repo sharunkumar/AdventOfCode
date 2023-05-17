f5435438ceb1668f132035b39902e30130ee3df7
Sharun <sharunksplus@gmail.com>
2023-05-17 02:08:47 -0400

    parsed the input

--- typescript/src/solutions/day-15-beacon-exclusion-zone-1.ts
+++ typescript/src/solutions/day-15-beacon-exclusion-zone-1.ts
@@ -1,9 +1,17 @@
-import { Solution } from "../utils";
+import { Point, Solution } from "../utils";
 
 export default class BeaconExclusionZone extends Solution {
     solve(input: string) {
-        this.get_lines(input)
+        console.clear()
+        let coords = this.get_lines(input)
+            .map(line => [...line.matchAll(/\-?\d+/g)].map(m => Number(m[0])))
+            .map(([x1, y1, x2, y2]) => [new Point(x1, y1), new Point(x2, y2)])
             .pipelog()
+
+        let dist = coords.map(([p1, p2]) => p1.manhattan_distance_to(p2))
+            .pipelog()
+
+        let Y = 10
     }
 
 }
