d326c881e4cfa8a2a34c1ff175ccf7532031bbb9
Sharun <sharunksplus@gmail.com>
2023-05-17 02:00:03 -0400

    added manhattan_distance_to in point

--- typescript/src/utils/index.ts
+++ typescript/src/utils/index.ts
@@ -87,4 +87,8 @@ export function manhattan_distance(x1: number, y1: number, x2: number, y2: numbe
 
 export class Point {
     constructor(public x: number, public y: number) { }
+
+    manhattan_distance_to(other: Point) {
+        return manhattan_distance(this.x, this.y, other.x, other.y)
+    }
 }
