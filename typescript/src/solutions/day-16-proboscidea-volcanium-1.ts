7ef6585b80915c4634ee1fd8cc6b7858df8f6a5c
Sharun <sharunksplus@gmail.com>
2023-05-17 20:51:51 -0400

    solution for part 1 (227ms)

--- typescript/src/solutions/day-16-proboscidea-volcanium-1.ts
+++ typescript/src/solutions/day-16-proboscidea-volcanium-1.ts
@@ -1,9 +1,11 @@
 import { Solution, regexMatch } from "../utils";
 
 type valve = string
-interface distance { [key: valve]: { [key: valve]: number } }
-interface valves { [key: valve]: number }
+type rate = number
+interface distance { [key: valve]: { [key: valve]: rate } }
+interface valves { [key: valve]: rate }
 interface tunnels { [key: valve]: valve[] }
+interface indices { [key: valve]: number }
 
 export default class ProboscideaVolcanium extends Solution {
     solve(input: string) {
@@ -19,8 +21,73 @@ export default class ProboscideaVolcanium extends Solution {
                 tunnels[valve] = targets
             })
 
-        for (let x in valves) {
-            console.log({ x, v: valves[x] })
+        for (let valve in valves) {
+            if (valve != "AA" && !valves[valve]) continue
+            if (valve != "AA") nonempty.push(valve)
+
+            dist[valve] = { "AA": 0 }
+            dist[valve][valve] = 0
+
+            let visited = [valve] as valve[]
+
+            let queue = [[0, valve] as [number, valve]]
+
+            while (queue.length > 0) {
+                let item = queue.shift()
+                if (!item) break
+                let [d, pos] = item
+
+                for (let i = 0; i < tunnels[pos].length; i++) {
+                    const nb = tunnels[pos][i];
+                    if (visited.includes(nb))
+                        continue
+                    visited.push(nb)
+                    if (valves[nb]) {
+                        dist[valve][nb] = d + 1
+                    }
+                    queue.push([d + 1, nb])
+                }
+            }
+
+            delete dist[valve][valve]
+            if (valve != "AA") {
+                delete dist[valve]["AA"]
+            }
         }
+
+        let indices: indices = {}
+
+        nonempty.forEach((x, idx) => {
+            indices[x] = idx
+        })
+
+        let cache = new Map<string, number>()
+
+        function dfs(time: number, valve: valve, bitmask: number) {
+            let hit = cache.get(`${time}, ${valve}, ${bitmask}`)
+            if (hit) return hit
+
+            let maxval = 0
+
+            for (let nb in dist[valve]) {
+                let bit = 1 << indices[nb] // open the valve in the bitmask
+                if (bitmask & bit) continue
+                let remaining = time - dist[valve][nb] - 1 //time to open the valve
+                if (remaining <= 0) continue
+
+                maxval = Math.max(maxval, dfs(remaining, nb, bitmask | bit) + valves[nb] * remaining)
+
+                if (Number.isNaN(maxval)) {
+                    throw new Error("wtf")
+                };
+
+            }
+
+            cache.set(`${time}, ${valve}, ${bitmask}`, maxval)
+            return maxval
+        }
+
+        return dfs(30, "AA", 0)
     }
+
 }
