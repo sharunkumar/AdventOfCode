da932f647902e3133f013aa392f6cf4b9a535771
Sharun <sharunksplus@gmail.com>
2023-06-20 03:40:22 -0400

    minor update on previous solution

--- typescript/src/utils/Point3D.ts
+++ typescript/src/utils/Point3D.ts
@@ -7,19 +7,19 @@ export function hash(point: point3d) {
 }
 
 export function minp(p1: point3d, p2: point3d) {
-    return [Math.min(p1[0], p2[0]), Math.min(p1[1], p2[1]), Math.min(p1[2], p2[2])] as point3d
+    return p3d(Math.min(p1[0], p2[0]), Math.min(p1[1], p2[1]), Math.min(p1[2], p2[2]))
 }
 
 export function maxp(p1: point3d, p2: point3d) {
-    return [Math.max(p1[0], p2[0]), Math.max(p1[1], p2[1]), Math.max(p1[2], p2[2])] as point3d
+    return p3d(Math.max(p1[0], p2[0]), Math.max(p1[1], p2[1]), Math.max(p1[2], p2[2]))
 }
 
 export function add(a: point3d, b: point3d) {
-    return [a[0] + b[0], a[1] + b[1], a[2] + b[2]] as point3d
+    return p3d(a[0] + b[0], a[1] + b[1], a[2] + b[2])
 }
 
 export function mul(a: point3d, b: number) {
-    return [a[0] * b, a[1] * b, a[2] * b] as point3d
+    return p3d(a[0] * b, a[1] * b, a[2] * b)
 }
 
 export function p3d(x: number, y: number, z: number) {
