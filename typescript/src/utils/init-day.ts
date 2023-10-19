70ee0dcd94efeca5a0521f727846d607ea3877ce
Sharun <sharunksplus@gmail.com>
2023-10-19 19:54:49 -0400

    update title-case to the latest version

--- typescript/src/utils/init-day.ts
+++ typescript/src/utils/init-day.ts
@@ -1,52 +1,53 @@
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import { regexMatch } from ".";
-import { titleCase } from "title-case";
 import path from "path";
 
-let [_1, _2, ...arg] = process.argv;
+import("title-case").then(({ titleCase }) => {
+  let [_1, _2, ...arg] = process.argv;
 
-let title_with_day = arg.join(" ").replace(/Day (\d):/g, "Day 0$1:");
+  let title_with_day = arg.join(" ").replace(/Day (\d):/g, "Day 0$1:");
 
-let title = titleCase(regexMatch(title_with_day, /: .*/g).join("")).replace(": ", "");
+  let title = titleCase(regexMatch(title_with_day, /: .*/g).join(""), {
+    smallWords: new Set<string>(),
+  }).replace(": ", "");
 
-let className = title.replace(/ /g, "");
+  let className = title.replace(/ /g, "");
 
-let file_name = title_with_day.replace(":", "").replace(/ /g, "-").toLowerCase();
+  let file_name = title_with_day.replace(":", "").replace(/ /g, "-").toLowerCase();
 
-let file_path = path.join(".", "src", "solutions", file_name);
+  let file_path = path.join(".", "src", "solutions", file_name);
 
-let test_file_path = path.join(".", "src", "tests", file_name);
+  let test_file_path = path.join(".", "src", "tests", file_name);
 
-let template_path = path.join(".", "src", "utils", "TemplateSolution.ts");
+  let template_path = path.join(".", "src", "utils", "TemplateSolution.ts");
 
-let test_template_path = path.join(".", "src", "utils", "TemplateTest.ts");
+  let test_template_path = path.join(".", "src", "utils", "TemplateTest.ts");
 
-let sample_input_path = path.join(".", "io", className + ".txt");
+  let sample_input_path = path.join(".", "io", className + ".txt");
 
-let test_sample_input_path = path.join(".", "src", "tests", "io", file_name + ".input.txt");
+  let test_sample_input_path = path.join(".", "src", "tests", "io", file_name + ".input.txt");
 
-let extensions = ["-1.ts", "-2.ts"];
+  let extensions = ["-1.ts", "-2.ts"];
 
-let test_extensions = [".test.ts"];
+  let test_extensions = [".test.ts"];
 
-let file_content = readFileSync(template_path)
-  .toString()
-  .replace(/TemplateSolution/g, className);
+  let file_content = readFileSync(template_path)
+    .toString()
+    .replace(/TemplateSolution/g, className);
 
-let test_file_content = readFileSync(test_template_path)
-  .toString()
-  .replace(/day 1/g, title_with_day)
-  .replace(/day-01-calorie-counting/g, file_name);
+  let test_file_content = readFileSync(test_template_path)
+    .toString()
+    .replace(/day 1/g, title_with_day)
+    .replace(/day-01-calorie-counting/g, file_name);
 
-extensions.forEach((e) => createFile(file_path + e, file_content));
+  extensions.forEach((e) => createFile(file_path + e, file_content));
 
-test_extensions.forEach((e) => createFile(test_file_path + e, test_file_content));
+  test_extensions.forEach((e) => createFile(test_file_path + e, test_file_content));
 
-createFile(sample_input_path);
+  createFile(sample_input_path);
 
-createFile(test_sample_input_path);
-
-// console.log({ className, file_name, file_path })
+  createFile(test_sample_input_path);
+});
 
 function createFile(file_path: string, file_content: string = "") {
   if (!existsSync(file_path)) {
