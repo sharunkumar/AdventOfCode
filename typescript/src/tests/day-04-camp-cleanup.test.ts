2dfebe541d2137bc50d89f8b2f3f3007d14cbc3f
Sharun <sharunksplus@gmail.com>
2023-10-19 01:41:57 -0400

    test for day 4

---
+++ typescript/src/tests/day-04-camp-cleanup.test.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import { default as part1 } from "../solutions/day-04-camp-cleanup-1";
+import { default as part2 } from "../solutions/day-04-camp-cleanup-2";
+import { readFileSync } from "fs";
+
+describe("Day 04: Camp Cleanup", () => {
+  let input: string;
+  beforeAll(() => {
+    const input_file_name = __filename.replace(/\.test\.ts$/, ".input.txt").replace(/tests/, "tests/io");
+    input = readFileSync(input_file_name, "utf-8");
+  });
+  test("part 1", () => {
+    let output = new part1().solve(input);
+    expect(output).toBe(2);
+  });
+  test("part 2", () => {
+    let output = new part2().solve(input);
+    expect(output).toBe(4);
+  });
+});
