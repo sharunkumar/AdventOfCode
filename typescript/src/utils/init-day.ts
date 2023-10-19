1cf6322884e413ae4b0687e57d98357858749381
Sharun <sharunksplus@gmail.com>
2023-10-19 01:30:15 -0400

    crated test generator in init day script

--- typescript/src/utils/init-day.ts
+++ typescript/src/utils/init-day.ts
@@ -15,19 +15,36 @@ let file_name = title_with_day.replace(":", "").replace(/ /g, "-").toLowerCase()
 
 let file_path = path.join(".", "src", "solutions", file_name);
 
+let test_file_path = path.join(".", "src", "tests", file_name);
+
 let template_path = path.join(".", "src", "utils", "TemplateSolution.ts");
+
+let test_template_path = path.join(".", "src", "utils", "TemplateTest.ts");
 
 let sample_input_path = path.join(".", "io", className + ".txt");
 
+let test_sample_input_path = path.join(".", "src", "tests", "io", file_name + ".input.txt");
+
 let extensions = ["-1.ts", "-2.ts"];
+
+let test_extensions = [".test.ts"];
 
 let file_content = readFileSync(template_path)
   .toString()
   .replace(/TemplateSolution/g, className);
 
+let test_file_content = readFileSync(test_template_path)
+  .toString()
+  .replace(/day 1/g, title)
+  .replace(/day-01-calorie-counting/g, file_name);
+
 extensions.forEach((e) => createFile(file_path + e, file_content));
 
+test_extensions.forEach((e) => createFile(test_file_path + e, test_file_content));
+
 createFile(sample_input_path);
+
+createFile(test_sample_input_path);
 
 // console.log({ className, file_name, file_path })
 
