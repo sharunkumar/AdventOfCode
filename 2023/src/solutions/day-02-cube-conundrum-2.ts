35c7e199b45b53a8df82d742f7004daf60395942
Sharun <sharunksplus@gmail.com>
2023-12-03 15:03:16 -0500

    cleanup

--- 2023/src/solutions/day-02-cube-conundrum-2.ts
+++ 2023/src/solutions/day-02-cube-conundrum-2.ts
@@ -2,55 +2,47 @@ import { Solution } from "../utils"
 
 export default class CubeConundrum extends Solution {
   solve(input: string) {
-    return (
-      this.get_lines(input)
-        .map((line) => {
-          const [game, ...rest] = line.split(": ")
+    return this.get_lines(input)
+      .map((line) => {
+        const [game, ...rest] = line.split(": ")
 
-          const groups = rest[rest.length - 1].split("; ")
+        const groups = rest[rest.length - 1].split("; ")
 
-          let game_number = Number(game.split(" ")[1])
+        let game_number = Number(game.split(" ")[1])
 
-          return { game_number, groups }
-        })
-        .map(({ game_number, groups }) => {
-          const sets = groups
-            .map((g) =>
-              g.split(", ").map((ball) => {
-                const ball_data = ball.split(" ")
-                return {
-                  ball: ball_data[1],
-                  count: Number(ball_data[0]),
-                }
-              }),
-            )
-            .flat()
-          return { game_number, sets }
-        })
-        // .filter((g) => g.game_number == 3)
-        .map(({ game_number, sets }) => {
-          const fewest = {
-            red: 0,
-            green: 0,
-            blue: 0,
+        return { game_number, groups }
+      })
+      .map(({ game_number, groups }) => {
+        const sets = groups
+          .map((g) =>
+            g.split(", ").map((ball) => {
+              const ball_data = ball.split(" ")
+              return {
+                ball: ball_data[1],
+                count: Number(ball_data[0]),
+              }
+            }),
+          )
+          .flat()
+        return { game_number, sets }
+      })
+      .map(({ game_number, sets }) => {
+        const fewest = {
+          red: 0,
+          green: 0,
+          blue: 0,
+        }
+
+        for (let idx = 0; idx < sets.length; idx++) {
+          const set = sets[idx]
+          if (fewest[set.ball] < set.count) {
+            fewest[set.ball] = set.count
           }
+        }
 
-          for (let idx = 0; idx < sets.length; idx++) {
-            const set = sets[idx]
-            // console.log({ set })
-            if (fewest[set.ball] < set.count) {
-              fewest[set.ball] = set.count
-            }
-          }
-
-          // console.log({ fewest })
-
-          return { game_number, fewest }
-        })
-        .map(({ fewest }) => fewest.red * fewest.green * fewest.blue)
-        // .pipelog()
-        // .pipelog()
-        .sum()
-    )
+        return { game_number, fewest }
+      })
+      .map(({ fewest }) => fewest.red * fewest.green * fewest.blue)
+      .sum()
   }
 }
