f26e2394b05fe3b2596cb304947fc40ff10e1d33
Sharun <sharunksplus@gmail.com>
2023-12-03 14:36:41 -0500

    tests pass for part 1

--- 2023/src/solutions/day-02-cube-conundrum-1.ts
+++ 2023/src/solutions/day-02-cube-conundrum-1.ts
@@ -1,5 +1,47 @@
-import { Solution } from "../utils";
+import { Solution } from "../utils"
+
+const limits = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
 
 export default class CubeConundrum extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    return this.get_lines(input)
+      .map((line) => {
+        const [game, ...rest] = line.split(": ")
+
+        const groups = rest[rest.length - 1].split("; ")
+
+        let game_number = Number(game.split(" ")[1])
+
+        return { game_number, groups }
+      })
+      .map(({ game_number, groups }) => {
+        console.log(game_number)
+        const sets = groups
+          .map((g) =>
+            g.split(", ").map((ball) => {
+              const ball_data = ball.split(" ")
+              return {
+                ball: ball_data[1],
+                count: ball_data[0],
+              }
+            }),
+          )
+          .map((data) => {
+            return data.map((d) => {
+              return limits[d.ball] >= d.count
+            })
+          })
+          .flat()
+        return { game_number, sets }
+      })
+      .filter((games) => {
+        return !games.sets.includes(false)
+      })
+      .map((g) => g.game_number)
+      .sum()
+  }
 }
