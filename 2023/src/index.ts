13049cc02845a99f9d150df7c2e91fb2b8b3704a
Sharun <sharunksplus@gmail.com>
2023-12-03 14:37:37 -0500

    solution for part 1

--- 2023/src/index.ts
+++ 2023/src/index.ts
@@ -1,36 +1,37 @@
-import { existsSync, readFileSync, writeFileSync } from "fs";
-import { Solution } from "./utils";
-import path from "path";
-import Trebuchet from "./solutions/day-01-trebuchet-2";
+import { existsSync, readFileSync, writeFileSync } from "fs"
+import { Solution } from "./utils"
+import path from "path"
+import Trebuchet from "./solutions/day-01-trebuchet-2"
+import CubeConundrum from "./solutions/day-02-cube-conundrum-1"
 
-const solution: Solution = new Trebuchet();
+const solution: Solution = new CubeConundrum()
 
-const input_file_name = path.join(__dirname, "..", "io", `${solution.constructor.name}.txt`);
+const input_file_name = path.join(__dirname, "..", "io", `${solution.constructor.name}.txt`)
 
 if (!existsSync(input_file_name)) {
-  writeFileSync(input_file_name, "", "utf-8");
-  console.debug(`${input_file_name} created.`);
+  writeFileSync(input_file_name, "", "utf-8")
+  console.debug(`${input_file_name} created.`)
 }
 
-const input = readFileSync(input_file_name, "utf-8");
+const input = readFileSync(input_file_name, "utf-8")
 
 if (input == "") {
-  console.debug(`add input to ${input_file_name} to continue.`);
-  process.exit(0);
+  console.debug(`add input to ${input_file_name} to continue.`)
+  process.exit(0)
 }
 
-const start = Date.now();
+const start = Date.now()
 
-const output = solution.solve(input);
+const output = solution.solve(input)
 
-output instanceof Promise ? output.then(ending) : ending(output);
+output instanceof Promise ? output.then(ending) : ending(output)
 
 function ending(output: any) {
-  const end = Date.now();
+  const end = Date.now()
 
-  console.debug({ output });
+  console.debug({ output })
 
-  console.debug("solution runtime: " + (end - start) + "ms");
+  console.debug("solution runtime: " + (end - start) + "ms")
 }
 
 // writeFileSync(__dirname + "/io/output.txt", output.toString(), { encoding: "utf8" })
