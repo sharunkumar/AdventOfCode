790b26b6b9877165635fb038e4a850044b3f3760
Sharun <sharunksplus@gmail.com>
2023-12-04 01:14:50 -0500

    update tests

--- 2023/src/tests/day-04-scratchcards.test.ts
+++ 2023/src/tests/day-04-scratchcards.test.ts
@@ -11,10 +11,10 @@ describe("Day 04: Scratchcards", () => {
     input_1 = readFileSync(input_file_name_1, "utf-8")
     input_2 = readFileSync(input_file_name_2, "utf-8")
   })
-  // test("part 1", () => {
-  //   let output = new part1().solve(input_1)
-  //   expect(output).toBe(13)
-  // })
+  test("part 1", () => {
+    let output = new part1().solve(input_1)
+    expect(output).toBe(13)
+  })
   test("part 2", () => {
     let output = new part2().solve(input_2)
     expect(output).toBe(30)
