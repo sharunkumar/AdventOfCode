6821d7cdbcfde7074cefb71259bc23e82a5f6bde
Sharun <sharunksplus@gmail.com>
2023-12-05 23:31:16 -0500

    parse input

--- 2023/src/solutions/day-05-if-you-give-a-seed-a-fertilizer-1.ts
+++ 2023/src/solutions/day-05-if-you-give-a-seed-a-fertilizer-1.ts
@@ -1,5 +1,23 @@
-import { Solution } from "../utils"
+import { Solution, regexMatch } from "../utils"
 
 export default class IfYouGiveASeedAFertilizer extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const [seeds_data, ...rest] = this.get_blocks(input)
+
+    // console.log({ seeds: seeds_data, rest })
+
+    const seeds = regexMatch(seeds_data, /\d+/g).map(Number) //.pipelog()
+
+    // console.log({ seeds, rest })
+
+    const maps = rest
+      .map(this.get_blocks)
+      .map((block) => block.map(this.get_lines))
+      .flat()
+      .map(([first, ...eh]) => [first, eh.map((x) => regexMatch(x, /\d+/g).map(Number))].flat())
+
+    console.log({ seeds, maps })
+
+    return 0
+  }
 }
