0dcc7e40fd89a18758b12cd5993ae0e8e1b6fe0d
Sharun <sharunksplus@gmail.com>
2023-12-07 23:11:16 -0500

    tests passing. ref. https://youtu.be/clRDvO3H9fU

--- 2023/src/solutions/day-07-camel-cards-1.ts
+++ 2023/src/solutions/day-07-camel-cards-1.ts
@@ -1,5 +1,58 @@
+import { countBy, sortBy } from "lodash"
 import { Solution } from "../utils"
 
 export default class CamelCards extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    // console.log({ input })
+
+    const lets = new Map([
+      ["T", "A"],
+      ["J", "B"],
+      ["Q", "C"],
+      ["K", "D"],
+      ["A", "E"],
+    ])
+
+    const data = this.get_lines(input)
+      .map((line) => line.split(" "))
+      .map(([first, second]) => {
+        return {
+          cards: first,
+          bid: Number(second),
+        }
+      })
+      .map((c) => {
+        return {
+          ...c,
+          counts: countBy(
+            c.cards,
+            (s) => Array.from(c.cards).filter((a) => a == s).length,
+          ),
+        }
+      })
+      .map((c) => {
+        return {
+          ...c,
+          classification: (() => {
+            if (c.counts["5"]) return 6
+            if (c.counts["4"]) return 4
+            if (c.counts["3"]) {
+              if (c.counts["2"]) return 4
+              return 3
+            }
+            if (c.counts["2"] === 4) return 2
+            if (c.counts[2]) return 1
+            return 0
+          })(),
+          strength: Array.from(c.cards)
+            .map((char) => lets.get(char) || char)
+            .join(""),
+        }
+      })
+    // .pipelog()
+
+    return sortBy(data, ["classification", "strength"])
+      .map((c, idx) => c.bid * (idx + 1))
+      .sum()
+  }
 }
