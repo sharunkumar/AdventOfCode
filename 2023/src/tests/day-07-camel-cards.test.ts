8526a6c1f7a93088d2d39dcc8e01463df423f198
Sharun <sharunksplus@gmail.com>
2023-12-08 00:26:48 -0500

    check with day 7 by making it generic

--- 2023/src/tests/day-07-camel-cards.test.ts
+++ 2023/src/tests/day-07-camel-cards.test.ts
@@ -15,7 +15,7 @@ describe("Day 07: Camel Cards", () => {
       .replace(/tests/, "tests/io")
     input_1 = readFileSync(input_file_name_1, "utf-8")
     input_2 = readFileSync(input_file_name_2, "utf-8")
-    input_prod = readFileSync("io/CamelCards.txt", "utf-8")
+    input_prod = readFileSync(`io/${new part1().constructor.name}.txt`, "utf-8")
   })
   test("part 1", () => {
     let output = new part1().solve(input_1)
