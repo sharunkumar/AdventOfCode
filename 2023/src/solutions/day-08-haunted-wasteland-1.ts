5aa511ca29c95b72265accbb4de3e16877176eb4
Sharun <sharunksplus@gmail.com>
2023-12-08 16:42:07 -0500

    tests passing

--- 2023/src/solutions/day-08-haunted-wasteland-1.ts
+++ 2023/src/solutions/day-08-haunted-wasteland-1.ts
@@ -1,5 +1,33 @@
-import { Solution } from "../utils"
+import { Solution, regexMatch } from "../utils"
+import { LoopingIterator } from "../utils/iterator"
 
 export default class HauntedWasteland extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const [first, second] = this.get_blocks(input) //.pipelog()
+
+    const directions = new LoopingIterator(
+      first.split("").map((lr) => (lr == "L" ? 0 : 1)),
+    )
+
+    // console.log({ directions })
+
+    const map = new Map<string, string[]>(
+      this.get_lines(second)
+        .map((line) => regexMatch(line, /\w+/g))
+        .map(([a, b, c]) => [a, [b, c]] as [string, string[]]),
+    )
+
+    // console.log(map)
+
+    let running = "AAA"
+    let steps = 0
+    let i = 0
+
+    while (running !== "ZZZ") {
+      running = map.get(running)![directions.next()]
+      steps++
+    }
+
+    return steps
+  }
 }
