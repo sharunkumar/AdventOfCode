d055df2820958d71715166222eff49d0113fdcec
Sharun <sharunksplus@gmail.com>
2023-12-08 18:25:33 -0500

    copy over logic from 2023

--- 2022/src/utils/TemplateTest.ts
+++ 2022/src/utils/TemplateTest.ts
@@ -1,19 +1,36 @@
-import { default as part1 } from "../solutions/day-01-calorie-counting-1";
-import { default as part2 } from "../solutions/day-01-calorie-counting-2";
-import { readFileSync } from "fs";
+import { default as part1 } from "../solutions/day-01-calorie-counting-1"
+import { default as part2 } from "../solutions/day-01-calorie-counting-2"
+import { readFileSync } from "fs"
 
 describe("day 1", () => {
-  let input: string;
+  let input_1: string
+  let input_prod: string
+  let input_2: string
   beforeAll(() => {
-    const input_file_name = __filename.replace(/\.test\.ts$/, ".input.txt").replace(/tests/, "tests/io");
-    input = readFileSync(input_file_name, "utf-8");
-  });
-  test("part 1", () => {
-    let output = new part1().solve(input);
-    expect(output).toBe(null);
-  });
-  test("part 2", () => {
-    let output = new part2().solve(input);
-    expect(output).toBe(null);
-  });
-});
+    const input_file_name_1 = __filename
+      .replace(/\.test\.ts$/, ".input.1.txt")
+      .replace(/tests/, "tests/io")
+    const input_file_name_2 = __filename
+      .replace(/\.test\.ts$/, ".input.2.txt")
+      .replace(/tests/, "tests/io")
+    input_1 = readFileSync(input_file_name_1, "utf-8")
+    input_2 = readFileSync(input_file_name_2, "utf-8")
+    input_prod = readFileSync(`io/${new part1().constructor.name}.txt`, "utf-8")
+  })
+  test.todo("part 1", () => {
+    let output = new part1().solve(input_1)
+    expect(output).toBe(null)
+  })
+  test.todo("part 1 - prod", () => {
+    let output = new part1().solve(input_prod)
+    expect(output).toBe(null)
+  })
+  test.todo("part 2", () => {
+    let output = new part2().solve(input_2)
+    expect(output).toBe(null)
+  })
+  test.todo("part 2 - prod", () => {
+    let output = new part2().solve(input_prod)
+    expect(output).toBe(null)
+  })
+})
