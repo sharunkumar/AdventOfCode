2a87680a0e48db7a13a71dfe1f1ad9fb44a52ac5
Sharun <sharunksplus@gmail.com>
2023-12-07 19:15:23 -0500

    tests passing for part 1

--- 2023/src/solutions/day-06-wait-for-it-1.ts
+++ 2023/src/solutions/day-06-wait-for-it-1.ts
@@ -1,5 +1,24 @@
-import { Solution } from "../utils"
+import { Solution, range, regexMatch } from "../utils"
 
 export default class WaitForIt extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const [times, distances] = this.get_lines(input).map((line) => regexMatch(line, /\d+/g).map(Number))
+    // .pipelog()
+
+    const races = times.map((t, idx) => [t, distances[idx]]) //.pipelog()
+
+    const wins = races.map(([time, distance]) => {
+      let result = 0
+
+      range(time).forEach((hold) => {
+        if (hold * (time - hold) > distance) {
+          result += 1
+        }
+      })
+
+      return result
+    })
+
+    return wins.product()
+  }
 }
