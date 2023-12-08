d8b4009128ac953c8c4c7172a013f2d95dd49a04
Sharun <sharunksplus@gmail.com>
2023-12-07 23:33:13 -0500

    add test for part 1 prod

--- 2023/src/tests/day-07-camel-cards.test.ts
+++ 2023/src/tests/day-07-camel-cards.test.ts
@@ -4,6 +4,7 @@ import { readFileSync } from "fs"
 
 describe("Day 07: Camel Cards", () => {
   let input_1: string
+  let input_1_prod: string
   let input_2: string
   beforeAll(() => {
     const input_file_name_1 = __filename
@@ -13,11 +14,16 @@ describe("Day 07: Camel Cards", () => {
       .replace(/\.test\.ts$/, ".input.2.txt")
       .replace(/tests/, "tests/io")
     input_1 = readFileSync(input_file_name_1, "utf-8")
+    input_1_prod = readFileSync("io/CamelCards.txt", "utf-8")
     input_2 = readFileSync(input_file_name_2, "utf-8")
   })
   test("part 1", () => {
     let output = new part1().solve(input_1)
     expect(output).toBe(6440)
+  })
+  test("part 1 - prod", () => {
+    let output = new part1().solve(input_1_prod)
+    expect(output).toBe(248569531)
   })
   test("part 2", () => {
     let output = new part2().solve(input_2)
