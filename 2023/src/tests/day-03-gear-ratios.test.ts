7ccac0046477ee805ddbf34b8fd494aaa8d163ca
Sharun <sharunksplus@gmail.com>
2023-12-08 00:36:23 -0500

    update expects for day 3

--- 2023/src/tests/day-03-gear-ratios.test.ts
+++ 2023/src/tests/day-03-gear-ratios.test.ts
@@ -4,6 +4,7 @@ import { readFileSync } from "fs"
 
 describe("Day 03: Gear Ratios", () => {
   let input_1: string
+  let input_prod: string
   let input_2: string
   beforeAll(() => {
     const input_file_name_1 = __filename
@@ -14,13 +15,23 @@ describe("Day 03: Gear Ratios", () => {
       .replace(/tests/, "tests/io")
     input_1 = readFileSync(input_file_name_1, "utf-8")
     input_2 = readFileSync(input_file_name_2, "utf-8")
+    const ctor_name = new part1().constructor.name
+    input_prod = readFileSync(`io/${ctor_name}.txt`, "utf-8")
   })
   test("part 1", () => {
     let output = new part1().solve(input_1)
     expect(output).toBe(4361)
   })
+  test("part 1 - prod", () => {
+    let output = new part1().solve(input_prod)
+    expect(output).toBe(546563)
+  })
   test("part 2", async () => {
     let output = await new part2().solve(input_2)
     expect(output).toBe(467835)
+  })
+  test("part 2 - prod", async () => {
+    let output = await new part2().solve(input_prod)
+    expect(output).toBe(91031374)
   })
 })
