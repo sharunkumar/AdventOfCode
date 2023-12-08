2f047a25c75161663a69e097996dca5d3b1c9063
Sharun <sharunksplus@gmail.com>
2023-12-08 00:30:55 -0500

    check by re-creating tests for day 1

--- 2023/src/tests/day-01-trebuchet.test.ts
+++ 2023/src/tests/day-01-trebuchet.test.ts
@@ -4,6 +4,7 @@ import { readFileSync } from "fs"
 
 describe("Day 01: Trebuchet", () => {
   let input_1: string
+  let input_prod: string
   let input_2: string
   beforeAll(() => {
     const input_file_name_1 = __filename
@@ -14,13 +15,23 @@ describe("Day 01: Trebuchet", () => {
       .replace(/tests/, "tests/io")
     input_1 = readFileSync(input_file_name_1, "utf-8")
     input_2 = readFileSync(input_file_name_2, "utf-8")
+    const ctor_name = new part1().constructor.name
+    input_prod = readFileSync(`io/${ctor_name}.txt`, "utf-8")
   })
-  test("part 1", () => {
+  test.todo("part 1", () => {
     let output = new part1().solve(input_1)
-    expect(output).toBe(142)
+    expect(output).toBe(null)
   })
-  test("part 2", () => {
+  test.todo("part 1 - prod", () => {
+    let output = new part1().solve(input_prod)
+    expect(output).toBe(null)
+  })
+  test.todo("part 2", () => {
     let output = new part2().solve(input_2)
-    expect(output).toBe(281)
+    expect(output).toBe(null)
+  })
+  test.todo("part 2 - prod", () => {
+    let output = new part2().solve(input_prod)
+    expect(output).toBe(null)
   })
 })
