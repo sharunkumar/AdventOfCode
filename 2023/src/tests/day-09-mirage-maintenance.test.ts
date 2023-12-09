f25a9830c93e916dc3eadef72d0bfed7d7a2e527
Sharun <sharunksplus@gmail.com>
2023-12-09 10:12:25 -0500

    prod passing, updating tests

--- 2023/src/tests/day-09-mirage-maintenance.test.ts
+++ 2023/src/tests/day-09-mirage-maintenance.test.ts
@@ -29,8 +29,8 @@ describe("Day 09: Mirage Maintenance", () => {
     let output = new part2().solve(input_2)
     expect(output).toBe(2)
   })
-  test.todo("part 2 - prod", () => {
+  test("part 2 - prod", () => {
     let output = new part2().solve(input_prod)
-    expect(output).toBe(null)
+    expect(output).toBe(977)
   })
 })
