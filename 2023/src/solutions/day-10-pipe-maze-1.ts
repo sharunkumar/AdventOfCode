830c4694ca6b3c2c9447c4fe3cca56ab413e1759
Sharun <sharunksplus@gmail.com>
2023-12-19 16:27:03 -0600

    get the location of S

--- 2023/src/solutions/day-10-pipe-maze-1.ts
+++ 2023/src/solutions/day-10-pipe-maze-1.ts
@@ -2,8 +2,20 @@ import { Solution, numberc } from "../utils"
 
 export default class PipeMaze extends Solution {
   solve(input: string) {
-    this.get_lines(input)
-      .map((line) => line.split(""))
+    let start_i = 0
+    let start_j = 0
+    const matrix = this.get_lines(input)
+      .map((line, idxi) =>
+        line.split("").map((c, idxj) => {
+          if (c == "S") {
+            start_i = idxi
+            start_j = idxj
+          }
+          return c
+        }),
+      )
       .pipelog()
+
+    console.log({ start_i, start_j })
   }
 }
