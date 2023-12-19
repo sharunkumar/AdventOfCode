e0fa6a5059d6dee381686969972e5ea9f89a04eb
Sharun <sharunksplus@gmail.com>
2023-12-19 16:24:21 -0600

    parse input into matrix

--- 2023/src/solutions/day-10-pipe-maze-1.ts
+++ 2023/src/solutions/day-10-pipe-maze-1.ts
@@ -1,5 +1,9 @@
-import { Solution } from "../utils"
+import { Solution, numberc } from "../utils"
 
 export default class PipeMaze extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    this.get_lines(input)
+      .map((line) => line.split(""))
+      .pipelog()
+  }
 }
