172ae5727b05ede16a5b2cbe3c313ee81854a002
Sharun <sharunksplus@gmail.com>
2023-12-19 17:11:37 -0600

    tests passing ref. https://www.youtube.com/watch?v=r3i3XE9H4uw

--- 2023/src/solutions/day-10-pipe-maze-1.ts
+++ 2023/src/solutions/day-10-pipe-maze-1.ts
@@ -23,13 +23,70 @@ export default class PipeMaze extends Solution {
     await prisma.seenCoordinates.deleteMany()
 
     interface coord {
-      i: Number
-      j: Number
+      i: number
+      j: number
     }
 
     let q = [] as coord[]
 
-    console.log({ start_i, start_j })
+    // console.log({ start_i, start_j })
+
+    async function not_seen(i: number, j: number) {
+      return (await prisma.seenCoordinates.count({ where: { i, j } })) == 0
+    }
+
+    q.push({ i: start_i, j: start_j })
+    await prisma.seenCoordinates.create({ data: { i: start_i, j: start_j } })
+
+    while (q.length) {
+      const { i, j } = q.shift()!
+
+      const ch = matrix[i][j]
+
+      // console.log({ ch })
+
+      if (
+        i > 0 &&
+        "S|JL".includes(ch) &&
+        "|7F".includes(matrix[i - 1][j]) &&
+        (await not_seen(i - 1, j))
+      ) {
+        await prisma.seenCoordinates.create({ data: { i: i - 1, j } })
+        q.push({ i: i - 1, j })
+      }
+
+      if (
+        i < matrix.length - 1 &&
+        "S|7F".includes(ch) &&
+        "|JL".includes(matrix[i + 1][j]) &&
+        (await not_seen(i + 1, j))
+      ) {
+        await prisma.seenCoordinates.create({ data: { i: i + 1, j } })
+        q.push({ i: i + 1, j })
+      }
+
+      if (
+        j > 0 &&
+        "S-J7".includes(ch) &&
+        "-LF".includes(matrix[i][j - 1]) &&
+        (await not_seen(i, j - 1))
+      ) {
+        await prisma.seenCoordinates.create({ data: { i, j: j - 1 } })
+        q.push({ i: i, j: j - 1 })
+      }
+
+      if (
+        j < matrix[0].length - 1 &&
+        "S-LF".includes(ch) &&
+        "-J7".includes(matrix[i][j + 1]) &&
+        (await not_seen(i, j + 1))
+      ) {
+        await prisma.seenCoordinates.create({ data: { i, j: j + 1 } })
+        q.push({ i: i, j: j + 1 })
+      }
+
+      // console.log({ curr })
+    }
 
     return floor((await prisma.seenCoordinates.count()) / 2)
   }
