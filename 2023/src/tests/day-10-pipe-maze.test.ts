30d066dfdfb7143fcf51a4e8e4a46043538192eb
Sharun <sharunksplus@gmail.com>
2023-12-19 20:25:44 -0600

    added 3rd test for part 2

--- 2023/src/tests/day-10-pipe-maze.test.ts
+++ 2023/src/tests/day-10-pipe-maze.test.ts
@@ -7,6 +7,7 @@ describe("Day 10: Pipe Maze", () => {
   let input_prod: string
   let input_2_1: string
   let input_2_2: string
+  let input_2_3: string
   beforeAll(() => {
     const input_file_name_1 = __filename
       .replace(/\.test\.ts$/, ".input.1.txt")
@@ -17,9 +18,13 @@ describe("Day 10: Pipe Maze", () => {
     const input_file_name_2_2 = __filename
       .replace(/\.test\.ts$/, ".input.2-2.txt")
       .replace(/tests/, "tests/io")
+    const input_file_name_2_3 = __filename
+      .replace(/\.test\.ts$/, ".input.2-3.txt")
+      .replace(/tests/, "tests/io")
     input_1 = readFileSync(input_file_name_1, "utf-8")
     input_2_1 = readFileSync(input_file_name_2_1, "utf-8")
     input_2_2 = readFileSync(input_file_name_2_2, "utf-8")
+    input_2_3 = readFileSync(input_file_name_2_3, "utf-8")
     input_prod = readFileSync(`io/${new part1().constructor.name}.txt`, "utf-8")
   })
   test("part 1", async () => {
@@ -37,6 +42,10 @@ describe("Day 10: Pipe Maze", () => {
   test("part 2 - 2", async () => {
     let output = await new part2().solve(input_2_2)
     expect(output).toBe(8)
+  })
+  test("part 2 - 3", async () => {
+    let output = await new part2().solve(input_2_3)
+    expect(output).toBe(10)
   })
   test.todo("part 2 - prod", () => {
     let output = new part2().solve(input_prod)
