e230c431e43af17e540e05c88e88341859b53423
Sharun <sharunksplus@gmail.com>
2023-12-22 14:01:36 -0600

    prepare part 2

--- 2023/src/solutions/day-14-parabolic-reflector-dish-2.ts
+++ 2023/src/solutions/day-14-parabolic-reflector-dish-2.ts
@@ -1,5 +1,34 @@
 import { Solution } from "../utils"
 
 export default class ParabolicReflectorDish extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const dish = this.get_matrix(input)
+
+    tilt(dish)
+    return dish
+      .map((row, idx) =>
+        row.map((c) => (c == "O" ? dish.length - idx : 0)).sum(),
+      )
+      .sum()
+  }
+}
+
+function tilt(dish: string[][]) {
+  for (let i = 1; i < dish.length; i++) {
+    const row = dish[i]
+    row.forEach((rock, j) => {
+      if (rock == "O") {
+        let k = i - 1
+        while (k >= 0) {
+          if (dish[k][j] == ".") {
+            dish[k][j] = "O"
+            dish[k + 1][j] = "."
+            k--
+          } else {
+            break
+          }
+        }
+      }
+    })
+  }
 }
