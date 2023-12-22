33d3b5ddcf9685c48be91766416fddf5ed0c067c
Sharun <sharunksplus@gmail.com>
2023-12-21 20:02:17 -0600

    basic parsing

--- 2023/src/solutions/day-12-hot-springs-1.ts
+++ 2023/src/solutions/day-12-hot-springs-1.ts
@@ -1,5 +1,10 @@
-import { Solution } from "../utils"
+import { Solution, numberc } from "../utils"
 
 export default class HotSprings extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    this.get_lines(input)
+      .map((line) => line.split(" "))
+      .map(([first, second]) => [first, second.split(",").map(numberc)])
+      .pipelog()
+  }
 }
