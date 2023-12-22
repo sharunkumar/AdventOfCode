0b5617c8e820e746279ebfd89bf83aceb2790c53
Sharun <sharunksplus@gmail.com>
2023-12-22 12:34:28 -0600

    test pass for example

--- 2023/src/solutions/day-13-point-of-incidence-2.ts
+++ 2023/src/solutions/day-13-point-of-incidence-2.ts
@@ -23,11 +23,7 @@ export default class PointOfIncidence extends Solution {
       .map((inp) => {
         return {
           f: inp.factor,
-          arr: inp.matrix
-            .map((row) =>
-              row.join("").replaceAll("#", "0").replaceAll(".", "1"),
-            )
-            .map((s) => parseInt(s, 2)),
+          arr: inp.matrix.map((row) => row.join("")),
         }
       })
       .map((x) => {
@@ -42,17 +38,24 @@ function transpose(matrix: string[][]): string[][] {
   return matrix[0].map((_, i) => matrix.map((row) => row[i]))
 }
 
-function get_index(arr: number[]): number {
+function get_index(arr: string[]): number {
   for (let i = 1; i < arr.length; i++) {
-    if (equiv(arr.slice(0, i).reverse(), arr.slice(i))) return i
+    if (diff(arr.slice(0, i).reverse(), arr.slice(i)) == 1) return i
   }
   return NaN
 }
 
-function equiv(arr1: number[], arr2: number[]) {
-  if (arr1.length == 0 || arr2.length == 0) return false
-  for (let i = 0; i < Math.min(arr1.length, arr2.length); i++) {
-    if (arr1[i] !== arr2[i]) return false
-  }
-  return true
+function diff(arr1: string[], arr2: string[]): number {
+  if (arr1.length == 0 || arr2.length == 0) return 0
+
+  return arr1
+    .map((str, idx) => [str, arr2[idx]])
+    .filter(([first, second]) => second)
+    .map(([first, second]) =>
+      first
+        .split("")
+        .map((c, idx) => (second[idx] == c ? 0 : 1))
+        .sum(),
+    )
+    .sum()
 }
