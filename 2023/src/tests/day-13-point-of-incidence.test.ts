812f1a5666134037086a60f10b7ebaf6f8624606
Sharun <sharunksplus@gmail.com>
2023-12-22 12:35:26 -0600

    prod passing, updating expect for part 2

--- 2023/src/tests/day-13-point-of-incidence.test.ts
+++ 2023/src/tests/day-13-point-of-incidence.test.ts
@@ -29,8 +29,8 @@ describe("Day 13: Point of Incidence", () => {
     let output = await new part2().solve(input_2)
     expect(output).toBe(400)
   })
-  test.todo("part 2 - prod", async () => {
+  test("part 2 - prod", async () => {
     let output = await new part2().solve(input_prod)
-    expect(output).toBe(null)
+    expect(output).toBe(34224)
   })
 })
