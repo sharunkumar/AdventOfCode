bbf092fa43106a15a40c492623a7276cf13e4ac6
Sharun <sharunksplus@gmail.com>
2023-12-22 11:36:00 -0600

    update expect for prod

--- 2023/src/tests/day-13-point-of-incidence.test.ts
+++ 2023/src/tests/day-13-point-of-incidence.test.ts
@@ -23,7 +23,7 @@ describe("Day 13: Point of Incidence", () => {
   })
   test("part 1 - prod", async () => {
     let output = await new part1().solve(input_prod)
-    expect(output).toBe(null)
+    expect(output).toBe(34889)
   })
   test.todo("part 2", async () => {
     let output = await new part2().solve(input_2)
