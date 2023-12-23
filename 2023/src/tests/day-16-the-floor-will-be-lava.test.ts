271261bc1743a98c4fa14c24d17b2e33587ca9a9
Sharun <sharunksplus@gmail.com>
2023-12-22 20:32:34 -0600

    prod passing

--- 2023/src/tests/day-16-the-floor-will-be-lava.test.ts
+++ 2023/src/tests/day-16-the-floor-will-be-lava.test.ts
@@ -23,7 +23,7 @@ describe("Day 16: The Floor Will Be Lava", () => {
   })
   test("part 1 - prod", async () => {
     let output = await new part1().solve(input_prod)
-    expect(output).toBe(null)
+    expect(output).toBe(8539)
   })
   test.todo("part 2", async () => {
     let output = await new part2().solve(input_2)
