95f48362aa6b8aeba16d79040865f4626220bd8e
Sharun <sharunksplus@gmail.com>
2023-12-22 20:41:00 -0600

    prepare part 2

--- 2023/src/solutions/day-16-the-floor-will-be-lava-2.ts
+++ 2023/src/solutions/day-16-the-floor-will-be-lava-2.ts
@@ -1,5 +1,144 @@
-import { Solution } from "../utils"
+import { Solution, inclusive_between as ibw } from "../utils"
+
+interface direction {
+  di: number
+  dj: number
+}
+
+interface coord {
+  i: number
+  j: number
+}
+
+interface vector {
+  point: coord
+  direction: direction
+}
 
 export default class TheFloorWillBeLava extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const map = this.get_matrix(input) //.pipelog()
+    const floor = this.get_matrix(input) //.pipelog()
+
+    // this.print_matrix(map)
+    function m({ i, j }: coord) {
+      return map[i][j]
+    }
+
+    function f({ i, j }: coord, val: string | undefined = undefined) {
+      if (!val) return floor[i][j]
+      else {
+        floor[i][j] = val
+      }
+    }
+
+    const fill: vector[] = [
+      {
+        point: {
+          i: 0,
+          j: -1,
+        },
+        direction: {
+          di: 0,
+          dj: 1,
+        },
+      },
+    ]
+
+    const memory: string[] = []
+
+    while (fill.length) {
+      const v = fill.shift()!
+
+      const next: coord = {
+        i: v.point.i + v.direction.di,
+        j: v.point.j + v.direction.dj,
+      }
+
+      // console.log(m(v.point), { next })
+
+      f(v.point, "#")
+
+      if (ibw(next.i, 0, map.length - 1) && ibw(next.j, 0, map[0].length - 1)) {
+        const key = JSON.stringify(v)
+        if (memory.includes(key)) {
+          continue
+        } else {
+          memory.push(key)
+        }
+        if (m(next) == ".") {
+          fill.push({
+            direction: v.direction,
+            point: next,
+          })
+        } else if (m(next) == "|") {
+          if (v.direction.dj == 0) {
+            // same direction as flow
+            fill.push({ direction: v.direction, point: next })
+          } else {
+            // flow is right angle
+            fill.push({ point: next, direction: { di: -1, dj: 0 } })
+            fill.push({ point: next, direction: { di: 1, dj: 0 } })
+          }
+        } else if (m(next) == "-") {
+          if (v.direction.di == 0) {
+            // same direction as flow
+            fill.push({ direction: v.direction, point: next })
+          } else {
+            // flow is right angle
+            fill.push({ point: next, direction: { di: 0, dj: 1 } })
+            fill.push({ point: next, direction: { di: 0, dj: -1 } })
+          }
+        } else if (m(next) == "/") {
+          if (v.direction.di == 0) {
+            // horizontal
+            if (v.direction.dj > 0) {
+              // currently going right
+              fill.push({ direction: { di: -1, dj: 0 }, point: next })
+            } else {
+              // currently going left
+              fill.push({ direction: { di: 1, dj: 0 }, point: next })
+            }
+          } else {
+            // vertical
+            if (v.direction.di < 0) {
+              // currently going up
+              fill.push({ direction: { di: 0, dj: 1 }, point: next })
+            } else {
+              // currently going down
+              fill.push({ direction: { di: 0, dj: -1 }, point: next })
+            }
+          }
+        } else if (m(next) == "\\") {
+          if (v.direction.di == 0) {
+            // horizontal
+            if (v.direction.dj > 0) {
+              // currently going right
+              fill.push({ direction: { di: 1, dj: 0 }, point: next })
+            } else {
+              // currently going left
+              fill.push({ direction: { di: -1, dj: 0 }, point: next })
+            }
+          } else {
+            // vertical
+            if (v.direction.di < 0) {
+              // currently going up
+              fill.push({ direction: { di: 0, dj: -1 }, point: next })
+            } else {
+              // currently going down
+              fill.push({ direction: { di: 0, dj: 1 }, point: next })
+            }
+          }
+        }
+      }
+    }
+
+    // console.error("---")
+    // this.print_matrix(floor)
+
+    return floor
+      .map((row) => row.map((c) => (c == "#" ? 1 : 0)))
+      .flat()
+      .sum()
+  }
 }
