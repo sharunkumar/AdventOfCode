24f806bdd0fd7324a18e4da85fbed23d4f3dcab5
Sharun <sharunksplus@gmail.com>
2023-12-22 18:18:16 -0600

    tests passing

--- 2023/src/solutions/day-15-lens-library-2.ts
+++ 2023/src/solutions/day-15-lens-library-2.ts
@@ -1,10 +1,52 @@
-import { Solution } from "../utils"
+import { empty } from "@prisma/client/runtime/library"
+import { Solution, numberc } from "../utils"
+import { isEmpty } from "lodash"
 
 export default class LensLibrary extends Solution {
   solve(input: string) {
-    return input
+    const boxes = new Array<[string, number][]>(256).fill([])
+
+    const instructions = input
       .split(",")
-      .map((s) => HASH(s))
+      .map((ins) => ins.split(/[=-]/))
+      .map(
+        ([first, second]) =>
+          [first, second == "" ? NaN : numberc(second)] as [string, number],
+      )
+      .map(
+        ([first, second]) =>
+          [HASH(first), first, second] as [number, string, number],
+      )
+      .pipelog()
+      .forEach(([hash, label, power]) => {
+        if (isNaN(power)) {
+          // remove
+          if (!isEmpty(boxes[hash])) {
+            boxes[hash] = boxes[hash].filter((x) => x[0] !== label)
+          }
+        } else {
+          // add/update
+          if (isEmpty(boxes[hash])) {
+            boxes[hash] = [[label, power]]
+          } else {
+            if (boxes[hash].filter((x) => x[0] == label).length) {
+              // update
+              boxes[hash] = boxes[hash].map((x) =>
+                x[0] == label ? [label, power] : x,
+              )
+            } else {
+              boxes[hash].push([label, power])
+            }
+          }
+        }
+      })
+
+    return boxes
+      .map((arr, idx) => {
+        return arr.length == 0
+          ? 0
+          : (idx + 1) * arr.map((elem, idy) => (idy + 1) * elem[1]).sum()
+      })
       .sum()
   }
 }
