f714968f480af54ba85b4e73291e4dee1bf9e62e
Sharun <sharunksplus@gmail.com>
2023-12-23 19:40:53 -0600

    parse input

--- 2023/src/solutions/day-17-clumsy-crucible-1.ts
+++ 2023/src/solutions/day-17-clumsy-crucible-1.ts
@@ -1,5 +1,11 @@
-import { Solution } from "../utils"
+import { Solution, numberc } from "../utils"
 
 export default class ClumsyCrucible extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const map = this.get_matrix(input).map((row) =>
+      row.map((char) => numberc(char)),
+    )
+
+    this.print_matrix(map)
+  }
 }
