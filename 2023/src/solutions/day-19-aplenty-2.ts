800dd0794211ffae6708d33ea56b8cf721da6bab
Sharun <sharunksplus@gmail.com>
2023-12-24 15:02:02 -0600

    create files

---
+++ 2023/src/solutions/day-19-aplenty-2.ts
@@ -0,0 +1,5 @@
+import { Solution } from "../utils"
+
+export default class Aplenty extends Solution {
+  solve(input: string) {}
+}
---
+++ 2023/src/tests/day-19-aplenty.test.ts
@@ -0,0 +1,36 @@
+import { default as part1 } from "../solutions/day-19-aplenty-1"
+import { default as part2 } from "../solutions/day-19-aplenty-2"
+import { readFileSync } from "fs"
+
+describe("Day 19: Aplenty", () => {
+  let input_1: string
+  let input_prod: string
+  let input_2: string
+  beforeAll(() => {
+    const input_file_name_1 = __filename
+      .replace(/\.test\.ts$/, ".input.1.txt")
+      .replace(/tests/, "tests/io")
+    const input_file_name_2 = __filename
+      .replace(/\.test\.ts$/, ".input.2.txt")
+      .replace(/tests/, "tests/io")
+    input_1 = readFileSync(input_file_name_1, "utf-8")
+    input_2 = readFileSync(input_file_name_2, "utf-8")
+    input_prod = readFileSync(`io/${new part1().constructor.name}.txt`, "utf-8")
+  })
+  test.todo("part 1", async () => {
+    let output = await new part1().solve(input_1)
+    expect(output).toBe(null)
+  })
+  test.todo("part 1 - prod", async () => {
+    let output = await new part1().solve(input_prod)
+    expect(output).toBe(null)
+  })
+  test.todo("part 2", async () => {
+    let output = await new part2().solve(input_2)
+    expect(output).toBe(null)
+  })
+  test.todo("part 2 - prod", async () => {
+    let output = await new part2().solve(input_prod)
+    expect(output).toBe(null)
+  })
+})
