744269e21f115803642d7e5c5e1cedfdb4b2f7f9
Sharun <sharunksplus@gmail.com>
2023-12-24 14:15:07 -0600

    define directions and type

--- 2023/src/solutions/day-18-lavaduct-lagoon-1.ts
+++ 2023/src/solutions/day-18-lavaduct-lagoon-1.ts
@@ -1,11 +1,18 @@
 import { Solution } from "../utils"
 
-type direction = "L" | "R" | "U" | "D"
+const directions = {
+  L: [0, -1],
+  R: [0, 1],
+  U: [-1, 0],
+  D: [1, 0],
+}
 
 export default class LavaductLagoon extends Solution {
   solve(input: string) {
     this.get_lines(input)
-      .map((line) => line.split(" ") as [direction, string, string])
+      .map(
+        (line) => line.split(" ") as [keyof typeof directions, string, string],
+      )
       .map(([dir, num, hex]) => [dir, Number(num), hex])
       .pipelog()
   }
