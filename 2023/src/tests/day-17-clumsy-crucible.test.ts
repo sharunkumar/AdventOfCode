c96b4e00536a503f3f88c039f3d5a18b90f90174
Sharun <sharunksplus@gmail.com>
2023-12-23 22:02:28 -0600

    prod passing, updating tests

--- 2023/src/tests/day-17-clumsy-crucible.test.ts
+++ 2023/src/tests/day-17-clumsy-crucible.test.ts
@@ -29,8 +29,8 @@ describe("Day 17: Clumsy Crucible", () => {
     let output = await new part2().solve(input_2)
     expect(output).toBe(94)
   })
-  test.todo("part 2 - prod", async () => {
+  test("part 2 - prod", async () => {
     let output = await new part2().solve(input_prod)
-    expect(output).toBe(null)
+    expect(output).toBe(1389)
   })
 })
