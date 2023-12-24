2ee40d07ea99cb0b2ed0357d511ffbc0f7fdb6c7
Sharun <sharunksplus@gmail.com>
2023-12-24 14:34:17 -0600

    test passing. ref. https://youtu.be/bGWK76_e-LM

--- 2023/src/solutions/day-18-lavaduct-lagoon-1.ts
+++ 2023/src/solutions/day-18-lavaduct-lagoon-1.ts
@@ -1,19 +1,43 @@
 import { Solution } from "../utils"
 
 const directions = {
-  L: [0, -1],
-  R: [0, 1],
-  U: [-1, 0],
-  D: [1, 0],
+  L: [0, -1] as [number, number],
+  R: [0, 1] as [number, number],
+  U: [-1, 0] as [number, number],
+  D: [1, 0] as [number, number],
 }
 
 export default class LavaductLagoon extends Solution {
   solve(input: string) {
+    const points: [number, number][] = [[0, 0]]
+    let boundary = 0
+
     this.get_lines(input)
       .map(
         (line) => line.split(" ") as [keyof typeof directions, string, string],
       )
-      .map(([dir, num, hex]) => [dir, Number(num), hex])
-      .pipelog()
+      .map(([dir, num, hex]) => {
+        const n = Number(num)
+        boundary += n
+        return directions[dir].map((d) => d * n) as [number, number]
+      })
+      .forEach((x) => {
+        const [r, c] = points.at(-1)!
+        points.push([x[0] + r, x[1] + c])
+      })
+
+    let area =
+      Math.abs(
+        points
+          .map(
+            (point, i) =>
+              point[0] *
+              (points.at(i - 1)![1] - points.at((i + 1) % points.length)![1]),
+          )
+          .sum(),
+      ) / 2
+    let i = area - boundary / 2 + 1
+
+    return i + boundary
   }
 }
