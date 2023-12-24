25cf75f62227d16e7009a98acb3501ee7132adc5
Sharun <sharunksplus@gmail.com>
2023-12-24 13:54:58 -0600

    parse input

--- 2023/src/solutions/day-18-lavaduct-lagoon-1.ts
+++ 2023/src/solutions/day-18-lavaduct-lagoon-1.ts
@@ -1,5 +1,12 @@
 import { Solution } from "../utils"
 
+type direction = "L" | "R" | "U" | "D"
+
 export default class LavaductLagoon extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    this.get_lines(input)
+      .map((line) => line.split(" ") as [direction, string, string])
+      .map(([dir, num, hex]) => [dir, Number(num), hex])
+      .pipelog()
+  }
 }
