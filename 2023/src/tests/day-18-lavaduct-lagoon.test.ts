64e6d1cf9db56ef6b6130c10f03efd75e046a3c2
Sharun <sharunksplus@gmail.com>
2023-12-24 14:36:27 -0600

    prod passing, updating test

--- 2023/src/tests/day-18-lavaduct-lagoon.test.ts
+++ 2023/src/tests/day-18-lavaduct-lagoon.test.ts
@@ -21,9 +21,9 @@ describe("Day 18: Lavaduct Lagoon", () => {
     let output = await new part1().solve(input_1)
     expect(output).toBe(62)
   })
-  test.todo("part 1 - prod", async () => {
+  test("part 1 - prod", async () => {
     let output = await new part1().solve(input_prod)
-    expect(output).toBe(null)
+    expect(output).toBe(46359)
   })
   test.todo("part 2", async () => {
     let output = await new part2().solve(input_2)
