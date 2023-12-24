84da0553a26f44ce740c7f5fc02fbc060c1c69bd
Sharun <sharunksplus@gmail.com>
2023-12-24 15:17:22 -0600

    parse the parts

--- 2023/src/solutions/day-19-aplenty-1.ts
+++ 2023/src/solutions/day-19-aplenty-1.ts
@@ -1,5 +1,26 @@
-import { Solution } from "../utils"
+import { Solution, regexMatch } from "../utils"
+
+interface xmas {
+  x: number
+  m: number
+  a: number
+  s: number
+}
 
 export default class Aplenty extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const [first, second] = this.get_blocks(input)
+
+    const parts = this.get_lines(second)
+      .map((line) => regexMatch(line, /\d+/g).map(Number))
+      .map((n): xmas => {
+        return {
+          x: n[0],
+          m: n[1],
+          a: n[2],
+          s: n[3],
+        }
+      })
+      .pipelog()
+  }
 }
