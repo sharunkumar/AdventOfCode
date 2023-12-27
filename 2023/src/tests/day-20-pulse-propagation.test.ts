4019d9b35eab0de1e6a35536d31cd61ec5fa2e24
Sharun <sharunksplus@gmail.com>
2023-12-26 19:35:51 -0600

    update second example in tests

--- 2023/src/tests/day-20-pulse-propagation.test.ts
+++ 2023/src/tests/day-20-pulse-propagation.test.ts
@@ -3,23 +3,32 @@ import { default as part2 } from "../solutions/day-20-pulse-propagation-2"
 import { readFileSync } from "fs"
 
 describe("Day 20: Pulse Propagation", () => {
-  let input_1: string
+  let input_1_1: string
+  let input_1_2: string
   let input_prod: string
   let input_2: string
   beforeAll(() => {
-    const input_file_name_1 = __filename
-      .replace(/\.test\.ts$/, ".input.1.txt")
+    const input_file_name_1_1 = __filename
+      .replace(/\.test\.ts$/, ".input.1-1.txt")
+      .replace(/tests/, "tests/io")
+    const input_file_name_1_2 = __filename
+      .replace(/\.test\.ts$/, ".input.1-2.txt")
       .replace(/tests/, "tests/io")
     const input_file_name_2 = __filename
       .replace(/\.test\.ts$/, ".input.2.txt")
       .replace(/tests/, "tests/io")
-    input_1 = readFileSync(input_file_name_1, "utf-8")
+    input_1_1 = readFileSync(input_file_name_1_1, "utf-8")
+    input_1_2 = readFileSync(input_file_name_1_2, "utf-8")
     input_2 = readFileSync(input_file_name_2, "utf-8")
     input_prod = readFileSync(`io/${new part1().constructor.name}.txt`, "utf-8")
   })
-  test("part 1", async () => {
-    let output = await new part1().solve(input_1)
+  test("part 1 - 1", async () => {
+    let output = await new part1().solve(input_1_1)
     expect(output).toBe(32000000)
+  })
+  test("part 1 - 2", async () => {
+    let output = await new part1().solve(input_1_2)
+    expect(output).toBe(11687500)
   })
   test.todo("part 1 - prod", async () => {
     let output = await new part1().solve(input_prod)
