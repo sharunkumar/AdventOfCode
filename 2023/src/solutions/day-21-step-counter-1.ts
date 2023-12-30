18f39f70db0b8f926fba17fa1f54662d511aa5dc
Sharun <sharunksplus@gmail.com>
2023-12-29 21:22:27 -0600

    parse input

--- 2023/src/solutions/day-21-step-counter-1.ts
+++ 2023/src/solutions/day-21-step-counter-1.ts
@@ -1,5 +1,27 @@
-import { Solution } from "../utils"
+import { Solution, numberc } from "../utils"
+
+interface coord {
+  i: number
+  j: number
+}
 
 export default class StepCounter extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const [inp, steps] = this.get_blocks(input).map(numberc) as [string, number]
+
+    const map = this.get_matrix(inp)
+
+    let start: coord = { i: 0, j: 0 }
+
+    map.forEach((row, r) =>
+      row.forEach((char, c) => {
+        if (char === "S") {
+          start.i = r
+          start.j = c
+        }
+      }),
+    )
+
+    console.log(start)
+  }
 }
