b5c41d9cd55fd6bb883fd08fc09aaa612a5013ed
Sharun <sharunksplus@gmail.com>
2023-12-30 16:35:10 -0600

    prod passing for part 2. ref. https://youtu.be/9UOMZSL0JTg

--- 2023/src/solutions/day-21-step-counter-2.ts
+++ 2023/src/solutions/day-21-step-counter-2.ts
@@ -19,11 +19,11 @@ export default class StepCounter extends Solution {
     super()
   }
   solve(input: string) {
-    const map = this.get_matrix(input)
+    const grid = this.get_matrix(input)
 
     let start: coord = { i: 0, j: 0 }
 
-    map.forEach((row, r) =>
+    grid.forEach((row, r) =>
       row.forEach((char, c) => {
         if (char === "S") {
           start.i = r
@@ -32,39 +32,85 @@ export default class StepCounter extends Solution {
       }),
     )
 
-    const ans = new Set<string>()
-    const seen = new Set<string>()
+    const size = grid.length
+    const steps = this.steps
 
-    const key = (c: coord): string => `${c.i}#${c.j}`
-    const add = (c: coord) => ans.add(key(c))
-    const see = (c: coord) => seen.add(key(c))
-    const inbound = (c: coord): boolean =>
-      ibw(c.i, 0, map.length) && ibw(c.j, 0, map[0].length)
+    function fill(sr: number, sc: number, ss: number) {
+      const ans = new Set<string>()
+      const seen = new Set<string>()
 
-    const q = new Array<[coord, number]>(1).fill([start, this.steps])
+      seen.add(`${sr},${sc}`)
+      let q: Array<[number, number, number]> = [[sr, sc, ss]]
 
-    while (q.length) {
-      const [curr, step] = q.shift()!
+      while (q.length > 0) {
+        let [r, c, s] = q.shift()!
 
-      if (step % 2 == 0) add(curr)
-      if (step === 0) continue
-
-      for (const next of directions(curr)) {
-        const { i: nr, j: nc } = next
-        if (
-          nr < 0 ||
-          nr >= map.length ||
-          nc < 0 ||
-          nc >= map[0].length ||
-          map[nr][nc] == "#" ||
-          seen.has(key(next))
-        )
+        if (s % 2 === 0) {
+          ans.add(`${r},${c}`)
+        }
+        if (s === 0) {
           continue
-        see(next)
-        q.push([next, step - 1])
+        }
+
+        let neighbors: Array<[number, number]> = [
+          [r + 1, c],
+          [r - 1, c],
+          [r, c + 1],
+          [r, c - 1],
+        ]
+        for (let [nr, nc] of neighbors) {
+          if (
+            nr < 0 ||
+            nr >= grid.length ||
+            nc < 0 ||
+            nc >= grid[0].length ||
+            grid[nr][nc] === "#" ||
+            seen.has(`${nr},${nc}`)
+          ) {
+            continue
+          }
+          seen.add(`${nr},${nc}`)
+          q.push([nr, nc, s - 1])
+        }
       }
+
+      return ans.size
     }
 
-    return ans.size
+    const { i: sr, j: sc } = start
+
+    let grid_width = Math.floor(steps / size) - 1
+
+    let odd = Math.pow(Math.floor(grid_width / 2) * 2 + 1, 2)
+    let even = Math.pow(Math.floor((grid_width + 1) / 2) * 2, 2)
+
+    let odd_points = fill(sr, sc, size * 2 + 1)
+    let even_points = fill(sr, sc, size * 2)
+
+    let corner_t = fill(size - 1, sc, size - 1)
+    let corner_r = fill(sr, 0, size - 1)
+    let corner_b = fill(0, sc, size - 1)
+    let corner_l = fill(sr, size - 1, size - 1)
+
+    let small_tr = fill(size - 1, 0, Math.floor(size / 2) - 1)
+    let small_tl = fill(size - 1, size - 1, Math.floor(size / 2) - 1)
+    let small_br = fill(0, 0, Math.floor(size / 2) - 1)
+    let small_bl = fill(0, size - 1, Math.floor(size / 2) - 1)
+
+    let large_tr = fill(size - 1, 0, Math.floor((size * 3) / 2) - 1)
+    let large_tl = fill(size - 1, size - 1, Math.floor((size * 3) / 2) - 1)
+    let large_br = fill(0, 0, Math.floor((size * 3) / 2) - 1)
+    let large_bl = fill(0, size - 1, Math.floor((size * 3) / 2) - 1)
+
+    return (
+      odd * odd_points +
+      even * even_points +
+      corner_t +
+      corner_r +
+      corner_b +
+      corner_l +
+      (grid_width + 1) * (small_tr + small_tl + small_br + small_bl) +
+      grid_width * (large_tr + large_tl + large_br + large_bl)
+    )
   }
 }
--- 2023/src/tests/day-21-step-counter.test.ts
+++ 2023/src/tests/day-21-step-counter.test.ts
@@ -26,28 +26,7 @@ describe("Day 21: Step Counter", () => {
   test("part 1 - prod", async () => {
     expect(await new part1(64).solve(input_prod)).toBe(3709)
   })
-  test.todo("part 2 - 6", async () => {
-    expect(await new part2(6).solve(input_2)).toBe(16)
-  })
-  test.todo("part 2 - 10", async () => {
-    expect(await new part2(10).solve(input_2)).toBe(50)
-  })
-  test.todo("part 2 - 50", async () => {
-    expect(await new part2(50).solve(input_2)).toBe(1594)
-  })
-  test.todo("part 2 - 100", async () => {
-    expect(await new part2(100).solve(input_2)).toBe(6536)
-  })
-  test.todo("part 2 - 500", async () => {
-    expect(await new part2(500).solve(input_2)).toBe(167004)
-  })
-  test.todo("part 2 - 1000", async () => {
-    expect(await new part2(1000).solve(input_2)).toBe(668697)
-  })
-  test.todo("part 2 - 5000", async () => {
-    expect(await new part2(5000).solve(input_2)).toBe(16733044)
-  })
-  test.todo("part 2 - prod", async () => {
-    expect(await new part2(26501365).solve(input_prod)).toBe(null)
+  test("part 2 - prod", async () => {
+    expect(await new part2(26501365).solve(input_prod)).toBe(617361073602319)
   })
 })
