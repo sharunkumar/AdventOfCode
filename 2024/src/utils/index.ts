ff54d8e9bcf89175ec43f16268d48ed264452301
Sharun <sharun@sharunkumar.com>
2024-12-02 12:43:12 -0500

    fix(utils): remove commented out code

--- 2024/src/utils/index.ts
+++ 2024/src/utils/index.ts
@@ -37,11 +37,6 @@ export function product(x: number, y: number): number {
   return x * y;
 }
 
-// export function pipelog<T>(x: T): T {
-//     console.log(x)
-//     return x
-// }
-
 export function char_to_ascii(char: string) {
   return char.charCodeAt(0);
 }
