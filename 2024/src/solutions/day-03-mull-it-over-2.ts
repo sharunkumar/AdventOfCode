e1df76fe58c4de20a4042620f3e6d209d5bdb390
Sharun <sharun@sharunkumar.com>
2024-12-03 00:07:24 -0500

    part 1 solved

--- 2024/src/solutions/day-03-mull-it-over-2.ts
+++ 2024/src/solutions/day-03-mull-it-over-2.ts
@@ -1,5 +1,11 @@
-import { Solution } from "#/utils";
+import { regexMatch, Solution, sum } from "#/utils";
 
 export default class MullItOver extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const exp = regexMatch(input, /mul\(\d+,\d+\)/g)
+      .map((mul) => regexMatch(mul, /\d+/g).map((n) => parseInt(n)))
+      .map(([a, b]) => a * b);
+
+    return exp.reduce(sum);
+  }
 }
