79bf8aea914edb8f213d05f42fe387f6e964bdb2
Sharun <sharun@sharunkumar.com>
2024-12-05 00:10:14 -0500

    commit solutions

--- 2024/src/solutions/day-05-print-queue-2.ts
+++ 2024/src/solutions/day-05-print-queue-2.ts
@@ -1,5 +1,45 @@
 import { Solution } from "#/utils";
 
 export default class PrintQueue extends Solution {
-  solve(input: string) {}
+  solve(input: string) {
+    const [rulesSection, updatesSection] = this.get_blocks(input);
+    const rules = this.get_lines(rulesSection).map((rule) =>
+      rule.split("|").map(Number)
+    );
+    const updates = this.get_lines(updatesSection).map((update) =>
+      update.split(",").map(Number)
+    );
+
+    const isCorrectOrder = (update: number[]) => {
+      for (const [x, y] of rules) {
+        const indexX = update.indexOf(x);
+        const indexY = update.indexOf(y);
+        if (indexX !== -1 && indexY !== -1 && indexX > indexY) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    const correctOrder = (update: number[]) => {
+      return update.sort((a, b) => {
+        for (const [x, y] of rules) {
+          if (a === x && b === y) return -1;
+          if (a === y && b === x) return 1;
+        }
+        return 0;
+      });
+    };
+
+    const incorrectUpdates = updates.filter(
+      (update) => !isCorrectOrder(update)
+    );
+    const orderedIncorrectUpdates = incorrectUpdates.map(correctOrder);
+    const middlePages = orderedIncorrectUpdates.map(
+      (update) => update[Math.floor(update.length / 2)]
+    );
+    const result = middlePages.reduce((sum, page) => sum + page, 0);
+
+    return result;
+  }
 }
