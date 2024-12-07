5336bb4386dd4883cf4273709e0037413b9f2235
Sharun <sharun@sharunkumar.com>
2024-12-07 11:25:11 -0500

    init: Day 7: Bridge Repair

---
+++ 2024/src/solutions/day-07-bridge-repair-2.ts
@@ -0,0 +1,5 @@
+import { Solution } from "#/utils";
+
+export default class BridgeRepair extends Solution {
+  solve(input: string) {}
+}
---
+++ 2024/src/tests/day-07-bridge-repair.test.ts
@@ -0,0 +1,40 @@
+import { default as part1 } from "#/solutions/day-07-bridge-repair-1";
+import { default as part2 } from "#/solutions/day-07-bridge-repair-2";
+import { readFileSync } from "node:fs";
+import { describe, test, beforeAll, expect } from "bun:test";
+
+describe("Day 07: Bridge Repair", () => {
+  let input_1: string;
+  let input_prod: string;
+  let input_2: string;
+  beforeAll(() => {
+    const input_file_name_1 = __filename
+      .replace(/\.test\.ts$/, ".input.1.txt")
+      .replace(/tests/, "tests/io");
+    const input_file_name_2 = __filename
+      .replace(/\.test\.ts$/, ".input.2.txt")
+      .replace(/tests/, "tests/io");
+    input_1 = readFileSync(input_file_name_1, "utf-8");
+    input_2 = readFileSync(input_file_name_2, "utf-8");
+    input_prod = readFileSync(
+      `io/${new part1().constructor.name}.txt`,
+      "utf-8"
+    );
+  });
+  test.todo("part 1", async () => {
+    const output = await new part1().solve(input_1);
+    expect(output).toBe(null); // FIXME - part 1
+  });
+  test.todo("part 1 - prod", async () => {
+    const output = await new part1().solve(input_prod);
+    expect(output).toBe(null); // FIXME - part 1 - prod
+  });
+  test.todo("part 2", async () => {
+    const output = await new part2().solve(input_2);
+    expect(output).toBe(null); // FIXME - part 2
+  });
+  test.todo("part 2 - prod", async () => {
+    const output = await new part2().solve(input_prod);
+    expect(output).toBe(null); // FIXME - part 2 - prod
+  });
+});
