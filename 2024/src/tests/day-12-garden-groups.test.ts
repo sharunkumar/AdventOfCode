80e0c7237f4327d2ba54bc1981c1a81071bf9ecb
Sharun <sharun@sharunkumar.com>
2024-12-13 17:23:21 -0500

    remove fixmes

--- 2024/src/tests/day-12-garden-groups.test.ts
+++ 2024/src/tests/day-12-garden-groups.test.ts
@@ -25,18 +25,18 @@ const [input_1, input_2, input_prod] = await Promise.all(
 describe("Day 12: Garden Groups", () => {
   test("part 1", async () => {
     const output = await new part1().solve(input_1);
-    expect(output).toBe(1930); // FIXME - part 1
+    expect(output).toBe(1930);
   });
   test("part 1 - prod", async () => {
     const output = await new part1().solve(input_prod);
-    expect(output).toBe(1431316); // FIXME - part 1 - prod
+    expect(output).toBe(1431316);
   });
   test("part 2", async () => {
     const output = await new part2().solve(input_2);
-    expect(output).toBe(80); // FIXME - part 2
+    expect(output).toBe(80);
   });
   test("part 2 - prod", async () => {
     const output = await new part2().solve(input_prod);
-    expect(output).toBe(821428); // FIXME - part 2 - prod
+    expect(output).toBe(821428);
   });
 });
